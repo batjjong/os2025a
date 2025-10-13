3ffdfff0a0698668fe87a815ebe12001e7dfdfb9
batjjong <batjjong@gmail.com>
2025-10-13 12:09:30 +0900

    loops/main.go v0.1 random

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"bufio"
+	//"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+	//"log"
+	//"os"
+	//"strconv"
+	//"strings"
+	"math/rand"
 	//"reflect"
 )
 
@@ -22,19 +23,24 @@ func main() {
 
 		fmt.Println("원본", reflect.TypeOf(length))
 		fmt.Println("형변환", reflect.TypeOf(float64(length)))
+	*/
 
+	/*
 		var now time.Time = time.Now()
 		//var month time.Month = now.Month()
 		month := now.Month()
 		var day int = now.Day()
 		fmt.Println(month)
 		fmt.Println(day)
+	*/
 
+	/*
 		univ := "Go$ rocks$"
 		changer := strings.NewReplacer("$", "!")
 		changed := changer.Replace(univ)
 		fmt.Println(changed)
 	*/
+
 	/*
 		//쉐도잉 shadowing
 		//var fmt string = "inha"
@@ -44,26 +50,34 @@ func main() {
 		fmt.Println(int)
 	*/
 
-	//a, b := 10,9 //동시에 선언 할당 가능
+	/*
+		//a, b := 10,9 //동시에 선언 할당 가능
 
-	r := bufio.NewReader(os.Stdin)
-	i, err := r.ReadString('\n') //에러 무시
-	//fmt.Println(err)
-	if err != nil {
-		log.Fatal(err) //프로그램 중지하고 시간+에러 메세지 출력
-	}
 
-	i = strings.TrimSpace(i)                //trim
-	score, err := strconv.ParseFloat(i, 64) //형변환
-	if err != nil {
-		log.Fatal(err)
-	}
+		r := bufio.NewReader(os.Stdin)
+		i, err := r.ReadString('\n') //에러 무시
+		//fmt.Println(err)
+		if err != nil {
+			log.Fatal(err) //프로그램 중지하고 시간+에러 메세지 출력
+		}
 
-	if score >= 60 {
-		fmt.Println("pass")
-	} else {
-		fmt.Println("fail")
-	}
-	fmt.Println(i)
+		i = strings.TrimSpace(i)                //trim
+		score, err := strconv.ParseFloat(i, 64) //형변환
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if score >= 60 {
+			fmt.Println("pass")
+		} else {
+			fmt.Println("fail")
+		}
+		fmt.Println(i)
+	*/
+
+	//rand.Seed(10)
+
+	dice := rand.Intn(6) + 1
+	fmt.Println(dice)
 
 }
