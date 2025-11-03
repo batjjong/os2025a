6e5029e02f755e2d8de11b590506fabd396d1908
PAKA05 <choigoldwin03@gmail.com>
2025-11-03 12:09:36 +0900

    week10 array v0.5

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -5,15 +5,14 @@ import (
 )
 
 func main() {
-	//arrayBool := [2]bool{true, false} //배열 리터럴
-	arrayInt := [3]int{-1, 11, 7}
-	for i := 0; i < len(arrayInt); i++ {
-		//fmt.Println(i, arrayBool[i]) //runtime error 인덱스 값이 맞지 않음
-		fmt.Println(i, arrayInt[i])
-		fmt.Println()
-	}
 
-	//fmt.Println(reflect.TypeOf(arrayBool))
-	//fmt.Printf("%#v\n", arrayBool)
-	//fmt.Printf("%#v\n", arrayInt)
+	//number := [3]int{-1, 11, 7}
+	//for i := 0; i < len(number); i++ {
+	//	fmt.Println(i, number[i])
+	//}
+
+	numbers := [3]int{-9, 11, 7}
+	for i, number := range numbers {
+		fmt.Println(i, number)
+	}
 }
