d98e7a6a6deceb8a223a4836e39f6f710e175cb4
batjjong <batjjong@gmail.com>
2025-12-01 11:30:14 +0900

    week14 panic v0.1

--- week14/cmd/ch12-ex02/main.go
+++ week14/cmd/ch12-ex02/main.go
@@ -1,13 +1,10 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	//LIFO
-	defer fmt.Println("1st defer")
-	defer fmt.Println("2nd defer")
-	defer fmt.Println("3rd defer")
-	fmt.Println("main")
+	defer fmt.Println("프로그램 종료 - 정리")
+	fmt.Println("1. 프로그램 시작")
+	panic("심각한 에러 발생!")
+	fmt.Println("2. 이 줄은 실행되지 않음")
 }
